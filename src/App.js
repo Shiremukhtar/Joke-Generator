fd13995d07d3cffc41dbafaeda75a7111553f896
cuncu <cuncu@DESKTOP-PQ5FM4I>
2022-02-01 01:17:14 +0100

    update state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import { useState, useRef } from 'react';
-import './App.css';
-import useRandomJoke from './useRandomJoke';
+import { useState, useRef } from "react";
+import "./App.css";
+import useRandomJoke from "./useRandomJoke";
 
 function App() {
-const firstNameRef = useRef(null)
-const lastNameRef = useRef(null)
-const [firstName, setFirstName] = useState('')
-const [lastName, setLastName] = useState('')
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const [firstName, setFirstName] = useState("Cigaar");
+  const [lastName, setLastName] = useState("Shiidaad");
 
-const joke = useRandomJoke(firstName, lastName);
-const generateJoke = (e) => {
-e.preventDefault();
-setFirstName(firstNameRef.current.value);
-setLastName(lastNameRef.current.value)
-}
+  const joke = useRandomJoke(firstName, lastName);
+  const generateJoke = (e) => {
+    e.preventDefault();
+    setFirstName(firstNameRef.current.value);
+    setLastName(lastNameRef.current.value);
+  };
   return (
     <div className="App">
       <h1>Joke Generator</h1>
-      <div style={{width: '400px', backgroundColor: "lightcoral", justifyContent: 'center', alignContent: 'center', alignItems: 'center', margin: '5px auto'}}>
-        <h2 style={{ }}>{joke}</h2>
+      <div
+        style={{
+          width: "400px",
+          backgroundColor: "lightcoral",
+          justifyContent: "center",
+          alignContent: "center",
+          alignItems: "center",
+          margin: "5px auto",
+        }}
+      >
+        <h2 style={{}}>{joke}</h2>
       </div>
 
       <form>
